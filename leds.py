19099aedd8b86d86e085d240e4bf521d9cba8f04
DarCarolina <dona@gbmpaulovi.edu.ec>
2025-11-07 14:58:08 -0500

    encender y apagar

--- leds.py
+++ leds.py
@@ -1,3 +1,16 @@
 from machine import Pin
 led= Pin (12, Pin.OUT)
 led.value(1)  # Encender led
+led.off()
+
+led= Pin (13, Pin.OUT)
+led.value(2)  # Encender led
+led.off()
+
+led= Pin (18, Pin.OUT)
+led.value(3)  # Encender led
+led.off()
+
+led= Pin (19, Pin.OUT)
+led.value(4)  # Encender led
+led.on()
